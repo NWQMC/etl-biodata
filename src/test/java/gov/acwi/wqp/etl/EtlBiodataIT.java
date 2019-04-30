f14813e219c2e4cddf5aae0d240d4442b9720c65
ssoper <ssoper@usgs.gov>
2019-04-30 13:56:48 -0500

    End to end test is working...
    Next, get the processor tests up and running...

--- src/test/java/gov/acwi/wqp/etl/EtlBiodataIT.java
+++ src/test/java/gov/acwi/wqp/etl/EtlBiodataIT.java
@@ -16,11 +16,14 @@ public class EtlBiodataIT extends BiodataBaseFlowIT {
 	@Test
 	
 	// resources from nwis and biodata
-	@DatabaseSetup(		connection=CONNECTION_WQP,		value="classpath:/testResult/wqp/station/empty.xml")
-	@DatabaseSetup(		connection=CONNECTION_WQP,		value="classpath:/testData/nwis/station/nwisStation.xml")
-	@DatabaseSetup(		connection=CONNECTION_BIODATA,	value="classpath:/testData/biodata/station/bioShareBiodataSite.xml")
-	@DatabaseSetup(		connection=CONNECTION_BIODATA,	value="classpath:/testData/biodata/station/bioShareSample.xml")
-	@DatabaseSetup(		connection=CONNECTION_BIODATA,	value="classpath:/testData/biodata/station/bioShareSampleType.xml")
+	@DatabaseSetup( connection=CONNECTION_WQP, value="classpath:/testResult/wqp/station/empty.xml")
+	@DatabaseSetup( connection=CONNECTION_WQP, value="classpath:/testResult/wqp/activity/empty.xml")
+	@DatabaseSetup( connection=CONNECTION_WQP, value="classpath:/testData/nwis/station/nwisStation.xml")
+	@DatabaseSetup( connection=CONNECTION_BIODATA, value="classpath:/testData/biodata/station/bioShareBiodataSite.xml")
+	@DatabaseSetup( connection=CONNECTION_BIODATA, value="classpath:/testData/biodata/station/bioShareSample.xml")
+	@DatabaseSetup( connection=CONNECTION_BIODATA, value="classpath:/testData/biodata/station/bioShareSampleType.xml")
+	@DatabaseSetup( connection=CONNECTION_BIODATA, value="classpath:/testData/biodata/activity/biodataEffort.xml")
+	@DatabaseSetup( connection=CONNECTION_BIODATA, value="classpath:/testData/biodata/activity/biodataProject.xml")
 	
 	// base tables
 	@ExpectedDatabase( 
@@ -35,13 +38,22 @@ public class EtlBiodataIT extends BiodataBaseFlowIT {
 			assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED,
 			table=EXPECTED_DATABASE_TABLE_CHECK_TABLE,
 			query=BASE_EXPECTED_DATABASE_QUERY_CHECK_TABLE + "'station_swap_biodata'")
-	
+	@ExpectedDatabase(
+			connection=CONNECTION_INFORMATION_SCHEMA,
+			value="classpath:/testResult/wqp/activity/create.xml",
+			assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED,
+			table=EXPECTED_DATABASE_TABLE_CHECK_TABLE,
+			query=BASE_EXPECTED_DATABASE_QUERY_CHECK_TABLE + "'activity_swap_biodata'")
+
 	// <dataType>_swap_biodata tables
 	@ExpectedDatabase( 
 			value="classpath:/testResult/wqp/orgData/orgData.xml", 
 			assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED)
 	@ExpectedDatabase( 
 			value="classpath:/testResult/wqp/station/station_swap_biodata.xml", 
+			assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED)
+	@ExpectedDatabase(
+			value="classpath:/testResult/wqp/activity/activity.xml",
 			assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED)
 
 	// indexes
@@ -55,6 +67,11 @@ public class EtlBiodataIT extends BiodataBaseFlowIT {
 			assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED,
 			table=EXPECTED_DATABASE_TABLE_CHECK_INDEX,
 			query=BASE_EXPECTED_DATABASE_QUERY_CHECK_INDEX + "'station_swap_biodata'")
+	@ExpectedDatabase(
+			value="classpath:/testResult/wqp/activity/indexes/all.xml",
+			assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED,
+			table=EXPECTED_DATABASE_TABLE_CHECK_INDEX,
+			query=BASE_EXPECTED_DATABASE_QUERY_CHECK_INDEX + "'activity_swap_biodata'")
 
 	public void endToEndTest() {
 		try {
