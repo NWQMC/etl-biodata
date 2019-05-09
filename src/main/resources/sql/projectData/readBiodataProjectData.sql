c6cd36815b7008e1086decd163bf8fe41e8d5ef3
ssoper <ssoper@usgs.gov>
2019-05-09 09:08:56 -0500

    clean up

--- src/main/resources/sql/projectData/readBiodataProjectData.sql
+++ src/main/resources/sql/projectData/readBiodataProjectData.sql
@@ -1,12 +1,12 @@
 select distinct
     activity.data_source_id
-    ,project.dw_project_id -- PROJECT_DATA table project_id
+    ,project.dw_project_id
     ,activity.data_source
     ,activity.organization
     ,activity.organization_name
-    ,activity.project_id -- PROJECT_DATA table project_identifier
+    ,activity.project_id
     ,activity.project_name
-    ,project.project_abstract -- PROJECT_DATA table description
+    ,project.project_abstract
 
 from biodata.project
 	join activity_swap_biodata activity
