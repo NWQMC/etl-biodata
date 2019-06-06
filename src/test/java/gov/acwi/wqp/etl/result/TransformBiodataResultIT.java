5d4c904bfc61754159c41c3c0e0350d0bf002e1f
ssoper <ssoper@usgs.gov>
2019-06-06 15:48:22 -0500

    Biodata result flow is now working.

--- src/test/java/gov/acwi/wqp/etl/result/TransformBiodataResultIT.java
+++ src/test/java/gov/acwi/wqp/etl/result/TransformBiodataResultIT.java
@@ -6,6 +6,7 @@ import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import gov.acwi.wqp.etl.BiodataBaseFlowIT;
 import org.junit.Test;
 import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,32 @@ public class TransformBiodataResultIT extends BiodataBaseFlowIT {
             connection=CONNECTION_BIODATA,
             value="classpath:/testData/biodata/result/result.xml",
             assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED)
-
     public void transformBiodataResultStepTest() {
         try {
             JobExecution jobExecution = jobLauncherTestUtils.launchStep("transformBiodataResultStep", testJobParameters);
+            assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getLocalizedMessage());
+        }
+    }
+
+    @Test
+    @DatabaseSetup( connection=CONNECTION_BIODATA, value="classpath:/testData/biodata/result/taxonomicResult.xml")
+    @DatabaseSetup( connection=CONNECTION_BIODATA, value="classpath:/testData/biodata/result/taxonWide.xml")
+    @ExpectedDatabase(
+            connection=CONNECTION_BIODATA,
+            value="classpath:/testData/biodata/result/result.xml",
+            assertionMode=DatabaseAssertionMode.NON_STRICT_UNORDERED)
+    public void biodataResultFlowTest() {
+        Job biodataResultFlowTest = jobBuilderFactory
+                .get("biodataResultFlowTest")
+                .start(biodataResultFlow)
+                .build()
+                .build();
+        jobLauncherTestUtils.setJob(biodataResultFlowTest);
+        try {
+            JobExecution jobExecution = jobLauncherTestUtils.launchJob(testJobParameters);
             assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
         } catch (Exception e) {
             e.printStackTrace();
